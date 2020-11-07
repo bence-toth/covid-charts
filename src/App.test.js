b8504b080e7c03909a6ef08d99194e6919cd7af0
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-11-07 01:32:04 +0100

    Updated test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/7-day moving average of COVID-19 deaths per million people/i);
+  expect(titleElement).toBeInTheDocument();
 });
